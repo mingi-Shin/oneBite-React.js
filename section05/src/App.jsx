175b7ed6f4e5c6052ac87a58e318ef7fae9f0917
Raphael.Shin <shinseogyong0621@gmail.com>
2024-09-08 16:47:07 +0900

    5-2

---
+++ section05/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+//컴퍼넌트는 첫글자가 대문자 
+const Header = () => {
+  return  (
+    <header>
+      <h1>HEADER~</h1>
+    </header>
+  )
+}
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Wellcome to Raact World!</h1>    
+    </>
+  )
+}
+
+export default App
