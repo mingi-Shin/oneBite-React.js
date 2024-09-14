eefc1be273fc691922532d2a4f95796d0991c9e7
shinseogyong <108261108+shinseogyong@users.noreply.github.com>
2024-09-14 13:08:21 +0900

    0914

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useReducer } from "react";
 
 //Ver.2.0: 객체로 묶어 속성 뽑아쓰기 (가독성Goat)
 const Register = () => {
@@ -10,11 +10,14 @@ const Register = () => {
     country: "선택",
     bio: ""
   });
-  console.log(value);
+  
+  const inputRef = useRef();
 
-  // e.target은 이벤트가 발생한 HTML 요소
-  // e.target.name은 해당 입력 필드의 name 속성을 참조
-  // 현재의 value 객체를 복사한 후(유지), e.target.name에 해당하는 키를 e.target.value로 업데이트하여 새로운 상태를 설정
+  /**
+    e.target은 이벤트가 발생한 HTML 요소
+    e.target.name은 해당 입력 필드의 name 속성을 참조
+    현재의 value 객체를 복사한 후(유지), e.target.name에 해당하는 키를 e.target.value로 업데이트하여 새로운 상태를 설정;
+   */
   const onChange = (e) => {
     setValue({
       ...value, // 객체를 복사하지 않으면 키들이 유지되지 않고 새로운 객체로 덮어씌워진다. 그럼 undefined가 된다. 
@@ -22,13 +25,25 @@ const Register = () => {
     });
   }
   
+  const onSubmit = () => {
+    alert("제출합니다.");
+    if(value.name === ""){
+      //이름을 입력하는 DOM 요소에 포커스 하기
+      console.log("해당 DOM 요소: ", inputRef.current);
+      inputRef.current.focus();
+    }
+  }
+
+
+
+
   return (
     <>
       <div>
         <h1>Register 페이지</h1>
       </div>
       <div>
-        <input name="name" onChange={onChange} value={value.name} placeholder={value.name} /> 값: {value.name}
+        <input ref={inputRef} name="name" onChange={onChange} value={value.name} placeholder={value.name} /> 값: {value.name}
       </div>
       <div>
         <input name="birth" onChange={onChange} value={value.birth} type="date" />값: {value.birth}
@@ -46,6 +61,8 @@ const Register = () => {
         <textarea name="bio" onChange={onChange} value={value.bio} style={{whiteSpace:"pre-wrap"}} placeholder="내용" rows={7}></textarea>
         값: {value.bio}
       </div>
+
+      <button onClick={onSubmit}>제출</button>
     </>
   );
 }
@@ -54,4 +71,8 @@ export default Register;
 
 /**
   onChange함수로 입력값을 매개변수로 받아서 useState의 value 로 저장 -> input태그의 value={??}에 따라 고정 
+
+  ref={inputRef} 코드에 따라, 해당 코드가 쓰여진 태그(input)의 DOM이 inputRef변수에 저장된다
+  예로, 위에서 name태그의 DOM은 inputRef변수로 호출 가능
+
  */
